7e33c9674cfdde1ec9b23def22a1b72316546ae2
Rômulo Oliveira <romuloffall@gmail.com>
2022-05-19 16:53:42 -0300

    configs for cors

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const jsonServer = require("json-server");
 const cors = require('cors');
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
@@ -7,7 +13,6 @@ const middleware = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
 
-server.use(cors({ credentials: true, origin: "http://localhost/3000" }));
 server.use(router);
 server.use(middleware);
 server.listen(port, () => {
