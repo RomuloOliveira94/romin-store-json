1bec9bb9fd400341c8760bdf7e50bc8d3b1c016c
Rômulo Oliveira <romuloffall@gmail.com>
2022-05-19 16:56:46 -0300

    cors

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const jsonServer = require("json-server");
 const cors = require('cors');
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    server.use(cors());
-    next();
-});
 
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
@@ -13,6 +7,12 @@ const middleware = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
 
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    server.use(cors());
+    next();
+});
 server.use(router);
 server.use(middleware);
 server.listen(port, () => {
